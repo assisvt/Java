659b5d058405c64f449f8246f82451837b60d74f
Vítor Assis <contatoassisvt@gmail.com>
2023-09-13 10:44:21 -0300

    Create Disciplina.java

---
+++ Porject_05/Disciplina.java
@@ -0,0 +1,14 @@
+package aulaRevisao;
+
+public class Disciplina {
+	
+	public String nome;
+	public int codigo;
+	public int quantidade;
+	
+	
+	public String toString( ) {
+		return nome+ " - " + quantidade;
+	}
+
+}
