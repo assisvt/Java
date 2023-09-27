5d9291f05dc08c644fd4723069db10b643fed0ad
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:51:06 -0300

    Update Formula1.java

--- project_07/Formula1.java
+++ project_07/Formula1.java
@@ -1,1 +1,17 @@
+package project_07;
 
+public class Formula1 {
+	
+	private int voltas;
+
+	public int getVoltas() {
+		return voltas;
+	}
+
+	public void setVoltas(int voltas) {
+		this.voltas = voltas;
+	}
+	
+	
+
+}
