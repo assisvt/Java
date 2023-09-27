c8b73c4ba6dbfaa2e8226c268c6d30f60f508d37
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:51:36 -0300

    Update Principal.java

--- project_07/Principal.java
+++ project_07/Principal.java
@@ -1,1 +1,16 @@
+package project_07;
 
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Futebol f = new Futebol();
+		f.setCampeonato("Libertadores");
+		f.setPatrocinio("Nike");
+		f.setTime("Fluminense");
+		
+		System.out.println(f.toString());
+		
+	}
+
+}
