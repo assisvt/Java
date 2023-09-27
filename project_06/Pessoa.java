b41c52b92fa1c798b2049794bf4461a2c602427e
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 09:53:47 -0300

    Create Pessoa.java

---
+++ project_06/Pessoa.java
@@ -0,0 +1,18 @@
+package aula_10;
+
+public class Pessoa {
+	
+	private String nome;
+	public String data;
+	public int idade;
+	public String getNome() {
+		return nome.toUpperCase();
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+	
+}
