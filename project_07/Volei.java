4100b12aaf2c8c8eae66c8d2c17bdac29918b50a
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:50:53 -0300

    Update Volei.java

--- project_07/Volei.java
+++ project_07/Volei.java
@@ -1,1 +1,18 @@
+package project_07;
+
+public class Volei {
+	
+	private int sets;
+
+	public int getSets() {
+		return sets;
+	}
+
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+	
+	
+
+}
 
