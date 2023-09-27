590aaf833a39c91878fc313ad89ea6e61a10b9f3
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 09:55:06 -0300

    Create PF.java

---
+++ project_06/PF.java
@@ -0,0 +1,15 @@
+package aula_10;
+
+public class PF extends Pessoa{
+	
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
