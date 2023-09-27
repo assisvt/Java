82dacfdeefcb1eabeaecf08495f09bb095350d0f
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:50:17 -0300

    Update Esporte.java

--- project_07/Esporte.java
+++ project_07/Esporte.java
@@ -1,1 +1,27 @@
+package project_07;
+
+public class Esporte {
+	
+	private String campeonato;
+	private String patrocinio;
+	
+	public String getCampeonato() {
+		return campeonato;
+	}
+	public void setCampeonato(String campeonato) {
+		this.campeonato = campeonato;
+	}
+	public String getPatrocinio() {
+		return patrocinio;
+	}
+	public void setPatrocinio(String patrocinio) {
+		this.patrocinio = patrocinio;
+	}
+	
+	public String toString() {
+		return campeonato;
+	}
+	
+
+}
 
