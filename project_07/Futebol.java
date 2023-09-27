ab354af14cd6c038810700138ae6b19c9efceb09
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:50:31 -0300

    Update Futebol.java

--- project_07/Futebol.java
+++ project_07/Futebol.java
@@ -1,1 +1,17 @@
+package project_07;
+
+public class Futebol extends Esporte {
+	
+	private String time;
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+
+}
 
