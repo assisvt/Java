53101a59ec2d4e4d77c0181effadb969a3128ae1
Vítor Assis <contatoassisvt@gmail.com>
2023-09-27 10:51:21 -0300

    Update Natacao.java

--- project_07/Natacao.java
+++ project_07/Natacao.java
@@ -1,1 +1,16 @@
+package project_07;
 
+public class Natacao {
+	
+	private int distancia;
+
+	public int getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+	
+
+}
