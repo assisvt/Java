f7b38ef64a5c89bef711ced29879e051b530de60
Vítor Assis <contatoassisvt@gmail.com>
2023-11-01 10:07:25 -0300

    Update Principal.java

--- polimorfismo/Principal.java
+++ polimorfismo/Principal.java
@@ -1,1 +1,13 @@
+package polimorfismo;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		PF p1 = new PF();
+		p1.nome = "João";
+		p1.cpf = "03034304106";
+		p1.apresentarNome();
+	}
+
+}
 
